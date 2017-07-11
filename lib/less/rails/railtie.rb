b3ad32e1c913c7643dc27c90afd61212ee4c175b
Tony Miller <mcfiredrill@gmail.com>
2017-07-11 17:31:30 +0900

    fix deprecation warnings
    using https://github.com/metaskills/less-rails/pull/128/files#diff-998c29126ad83cee99c229f1d43307b5

--- lib/less/rails/railtie.rb
+++ lib/less/rails/railtie.rb
@@ -1,6 +1,6 @@
 require 'sprockets/railtie'
 
-module Less  
+module Less
   module Rails
     class Railtie < ::Rails::Railtie
       config.less = ActiveSupport::OrderedOptions.new
@@ -13,14 +13,23 @@ module Less
         require 'less'
         require 'less-rails'
         Sprockets::Engines #force autoloading
-        Sprockets.register_engine '.less', LessTemplate
       end
 
       initializer 'less-rails.before.load_config_initializers', :before => :load_config_initializers, :group => :all do |app|
         sprockets_env = app.assets || Sprockets
-        sprockets_env.register_preprocessor 'text/css', ImportProcessor
 
         config.assets.configure do |env|
+          if env.respond_to?(:register_transformer)
+            env.register_mime_type 'text/less', extensions: ['.less'], charset: :css
+            env.register_transformer 'text/less', 'text/css', ImportProcessor
+          end
+
+          if env.respond_to?(:register_engine)
+            args = ['.less', LessTemplate]
+            args << {mime_type: 'text/less', silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+            env.register_engine(*args)
+          end
+
           env.context_class.class_eval do
             class_attribute :less_config
             self.less_config = app.config.less
@@ -32,11 +41,11 @@ module Less
         assets_stylesheet_paths = app.config.assets.paths.select { |p| p && p.to_s.ends_with?('stylesheets') }
         app.config.less.paths.unshift(*assets_stylesheet_paths)
       end
-      
+
       initializer 'less-rails.setup_compression', :group => :all do |app|
         config.less.compress = app.config.assets.compress
       end
-      
+
     end
   end
 end
